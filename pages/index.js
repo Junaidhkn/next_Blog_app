e4e0d3aea4158c88dfcc88e70674cbb41b3a13f8
Junaidhkn <junaidhkniazi@gmail.com>
2022-12-26 21:43:54 +0500

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Layout from "../components/Layout.js"
 import FeaturedPost from "../components/FeaturedPost.js"
+import PostsSection from "../components/PostsSection.js"
 
 const HomePage = () => {
   return (
     <>
       <Layout>
         <FeaturedPost />
+        <PostsSection />
       </Layout>
     </>
   )

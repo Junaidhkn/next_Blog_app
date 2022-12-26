5778fa8bc73449758644a9f6e75bfd036d81f13b
Junaidhkn <junaidhkniazi@gmail.com>
2022-12-26 21:30:41 +0500

    commit

--- components/FeaturedPost.js
+++ components/FeaturedPost.js
@@ -1,25 +1,38 @@
-import React from 'react';
+import Image from "next/image.js";
 
 const featuredPost = () => {
 	return (
-		<div className=' bg-greenGray w-full'>
-			<div className='container flex justify-around'>
-				<div className='flex justify-center basis-9/12'>
-					<h2>Featured Posts</h2>
-					<div className='container '>
-						<div></div>
-						<div></div>
+		<div className='container flex bg-greenGray w-full justify-around'>
+
+			<div className='flex justify-center grow'>
+				<h2>Featured Posts</h2>
+				<div className='container '>
+					<div>
+						<span>React.js</span>
+						<h3>I have create a React Booking App.Here's what I have Learnt from it.Check it out'</h3>
+						<div>
+							<p>Name</p>
+							<p>Date</p>
+						</div>
+						<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione illo non consequatur error ipsa eligendi eos doloribus eius, obcaecati sed, voluptates culpa vel similique. Quos odit sit et nostrum asperiores? Alias aliquid nemo incidunt...</p>
 					</div>
-				</div>
-				<div className='line'></div>
-				<div className='flex basis-3/12'>
-					<h2>Popular all time</h2>
-					<div className='container '>
+					<div>
 						<div></div>
-						<div></div>
+						<Image></Image>
 					</div>
 				</div>
 			</div>
+
+			<div className='line'></div>
+
+			<div className='flex'>
+				<h2>Popular all time</h2>
+				<div className='container '>
+					<div></div>
+					<div></div>
+				</div>
+			</div>
+
 		</div>
 	);
 };

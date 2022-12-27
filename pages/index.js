dda468d887329e3b566ff11ae01487f76f06ae2b
Junaidhkn <junaidhkniazi@gmail.com>
2022-12-27 14:33:51 +0500

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,16 @@
-import Layout from "../components/Layout.js"
-import FeaturedPost from "../components/FeaturedPost.js"
-import PostsSection from "../components/PostsSection.js"
+import Layout from '../components/Layout.js';
+import FeaturedPost from '../components/FeaturedPost.js';
+import PostsSection from '../components/PostsSection.js';
 
 const HomePage = () => {
-  return (
-    <>
-      <Layout>
-        <FeaturedPost />
-        <PostsSection />
-      </Layout>
-    </>
-  )
-}
+	return (
+		<>
+			<Layout>
+				<FeaturedPost />
+				<PostsSection />
+			</Layout>
+		</>
+	);
+};
 
-export default HomePage
+export default HomePage;

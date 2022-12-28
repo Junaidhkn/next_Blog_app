f9ee6dd1c8a0ef6c0665c6ac3fb9eab4006a9f0f
Junaidhkn <junaidhkniazi@gmail.com>
2022-12-28 12:06:17 +0500

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,11 @@
-import Layout from '../components/Layout.js';
 import FeaturedPost from '../components/FeaturedPost.js';
 import Header from '../components/Header.js';
 
 const HomePage = () => {
 	return (
 		<>
-			<Layout>
-				<Header />
-				<FeaturedPost />
-			</Layout>
+			<Header />
+			<FeaturedPost />
 		</>
 	);
 };

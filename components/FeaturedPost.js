861066b687cf25eb69ed09ec68ef12244b5c727a
Junaidhkn <junaidhkniazi@gmail.com>
2022-12-30 22:21:28 +0500

    fixes

--- components/FeaturedPost.js
+++ components/FeaturedPost.js
@@ -1,36 +1,22 @@
 import Image from "next/image.js";
 import Link from "next/link.js";
 import PostsGrid from "./PostsGrid.js";
+import PopularPostsGrid from "./PopularPostsGrid.js";
 
 const featuredPost = ( props ) => {
+	const { posts, popularPosts } = props
 	return (
 		<main className="flex ">
 			<div className='container flex flex-col bg-greenGray w-full mt-12 mb-14 mx-20'>
 				<h2 className="text-3xl font-bold mb-10"><span className='bg-greenSuper text-neutral-50 text-2xl p-1'>Featured</span> Posts</h2>
-				<PostsGrid posts={props.posts} />
+				<PostsGrid posts={posts} />
 			</div>
 			{/* {hover:outline-1 hover:drop-shadow-xl hover:outline-emerald-100} */}
 			{/* Popular Posts */}
 
 			<div className='flex flex-col mt-20 w-96 mr-20 gap-5'>
 				<h2 className="text-2xl font-bold mb-10"><span className='bg-greenSuper text-neutral-50 text-2xl p-1'>Popular</span> Posts</h2>
-				<Link href='/' className="hover:outline outline-2 outline-emerald-100  hover:drop-shadow-md">
-					<div className="p-5 ">
-						<div className='container mt-8'>
-							<div className=" w-[430]">
-								<span className="p-1 bg-greenFooter text-xs">React.js</span>
-								<h3 className="mt-1 font-[600] leading-[150%] text-[17px]">I have create a React Booking App.Here's what I have Learnt from it.Check it out'</h3>
-								<div className="m-2 flex space-x-7 text-xs font-thin">
-									{/* Add Icons */}
-									<p>Junaid Khan</p>
-									<p>27/12/2022</p>
-								</div>
-								<p className="leading-[22.5px] text-[15px] w-[430]">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione illo non consequatur error ipsa el...</p>
-							</div>
-						</div>
-					</div>
-
-				</Link>
+				<PopularPostsGrid posts={popularPosts} />
 			</div>
 
 		</main>
--- components/PopularPostsGrid.js
+++ components/PopularPostsGrid.js
@@ -1,24 +1,15 @@
 import React from 'react'
+import PopularPostItem from './PopularPostItem.js'
 
 const PopularPostsGrid = () => {
+   const { popularPosts } = props
    return (
-      <div><Link href='/' className="hover:outline outline-2 outline-emerald-100  hover:drop-shadow-md">
-         <div className="p-5 ">
-            <div className='container mt-8'>
-               <div className=" w-[430]">
-                  <span className="p-1 bg-greenFooter text-xs">React.js</span>
-                  <h3 className="mt-1 font-[600] leading-[150%] text-[17px]">I have create a React Booking App.Here's what I have Learnt from it.Check it out'</h3>
-                  <div className="m-2 flex space-x-7 text-xs font-thin">
-                     {/* Add Icons */}
-                     <p>Junaid Khan</p>
-                     <p>27/12/2022</p>
-                  </div>
-                  <p className="leading-[22.5px] text-[15px] w-[430]">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione illo non consequatur error ipsa el...</p>
-               </div>
-            </div>
-         </div>
+      <ul>
+         {popularPosts.map( ( post ) => {
+            <PopularPostItem key={post.slug} post={popularPosts} />
+         } )}
+      </ul>
 
-      </Link></div>
    )
 }
 

ac9ba4ae90e29f400e761144c40bd19cc0f5621b
Junaidhkn <junaidhkniazi@gmail.com>
2022-12-30 22:31:43 +0500

    static props

--- components/PostItem.js
+++ components/PostItem.js
@@ -1,30 +1,42 @@
 import React from 'react'
 
 const PostItem = () => {
+   const { tag, title, image, excerpt, date, slug } = props.post;
+
+   const formattedDate = new Date( date ).toLocaleDateString( 'en-US', {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric',
+   } );
+
+   const imagePath = `/images/posts/${slug}/${image}`;
+   const linkPath = `/posts/${slug}`;
    return (
-      <div><Link href='/' className="hover:outline outline-2 outline-emerald-100  hover:drop-shadow-md">
-         <div className="flex justify-evenly gap-8 mb-10 pb-10">
-            <div className='flex flex-col w-96 h-64'>
-               <div className='container mt-8'>
-                  <div className=" w-[430]">
-                     <span className="p-1 bg-greenFooter text-xs">React.js</span>
-                     {/* <h3 className="mt-1  text-3xl font-[600] leading-[37.5px] text-[27px]">{title}</h3> */}
-                     <div className="m-2 mt-3 mb-8 flex space-x-7 text-sm font-thin">
-                        {/* Add Icons */}
-                        <p>Junaid Hassan Khan</p>
-                        <p>27/12/2022</p>
+      <div>
+         <Link href={linkPath} className="hover:outline outline-2 outline-emerald-100  hover:drop-shadow-md">
+            <div className="flex justify-evenly gap-8 mb-10 pb-10">
+               <div className='flex flex-col w-96 h-64'>
+                  <div className='container mt-8'>
+                     <div className=" w-[430]">
+                        <span className="p-1 bg-greenFooter text-xs">{tag}</span>
+                        <h3 className="mt-1  text-3xl font-[600] leading-[37.5px] text-[27px]">{title}</h3>
+                        <div className="m-2 mt-3 mb-8 flex space-x-7 text-sm font-thin">
+                           {/* Add Icons */}
+                           <p>Junaid Hassan Khan</p>
+                           <p>{date}</p>
+                        </div>
+                        <p className="leading-[22.5px] text-[15px] w-[430]">{excerpt}</p>
                      </div>
-                     <p className="leading-[22.5px] text-[15px] w-[430]">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione illo non consequatur error ipsa eligendi eos doloribus eius, obcaecati sed, voluptates culpa vel similique. Quos odit sit et nostrum asperiores? Alias aliquid nemo incidunt...</p>
                   </div>
                </div>
+               <div className="relative w-[350px] h-[250px] mt-20">
+                  <div className="absolute w-[150px] h-[171px] -left-[20px] -top-[20px] z-0 opacity-80 bg-greenNav"></div>
+                  <Image src={imagePath} className="absolute w-full h-full z-10" width={300} height={230} />
+               </div>
             </div>
-            <div className="relative w-[350px] h-[250px] mt-20">
-               <div className="absolute w-[150px] h-[171px] -left-[20px] -top-[20px] z-0 opacity-80 bg-greenNav"></div>
-               <Image src='/images/blog.jpg' className="absolute w-full h-full z-10" width={300} height={230} />
-            </div>
-         </div>
 
-      </Link></div>
+         </Link>
+      </div>
    )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,29 @@
 import FeaturedPost from '../components/FeaturedPost.js';
 import Header from '../components/Header.js';
+import { getFeaturedPosts, getPopularPosts } from '../helper/utils.js';
 
-const HomePage = () => {
+const HomePage = ( props ) => {
+	const { posts, popularPosts } = props
 	return (
 		<>
 			<Header />
-			<FeaturedPost />
+			<FeaturedPost posts={posts} popularPosts={popularPosts} />
 		</>
 	);
 };
 
 export default HomePage;
+
+
+
+export function getStaticProps () {
+	const featuredPosts = getFeaturedPosts();
+	const popularPosts = getPopularPosts()
+
+	return {
+		props: {
+			posts: featuredPosts,
+			popularPosts: popularPosts
+		},
+	};
+}

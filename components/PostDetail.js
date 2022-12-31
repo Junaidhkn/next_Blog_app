1d16ec8604bb80dafb33ec7f1387d36dc41666e6
Junaidhkn <junaidhkniazi@gmail.com>
2022-12-31 18:34:18 +0500

    fixes

--- components/PostDetail.js
+++ components/PostDetail.js
@@ -17,7 +17,7 @@ function PostDetail ( props ) {
             const image = node.children[0];
 
             return (
-               <div>
+               <div className='w-full max-w-[600px] my-5 mx-auto'>
                   <Image
                      src={`/images/posts/${post.slug}/${image.properties.src}`}
                      alt={image.alt}
@@ -45,9 +45,13 @@ function PostDetail ( props ) {
    };
 
    return (
-      <article>
-         <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
-      </article>
+      <main className='mx-20 p-4 my-5 font-[400] leading-[22.5px] text-[15px] font-[#666666]'>
+         <h1 className="mt-1 mb-2 text-5xl font-[600]text-[27px]">{post.title}</h1>
+         <span className="p-1 bg-greenFooter text-xs">{post.tag}</span>
+         <article>
+            <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
+         </article>
+      </main>
    );
 }
 

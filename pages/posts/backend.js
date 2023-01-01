c1e67f90be4772b946b827ac6b0c894b8dacaeff
Junaidhkn <junaidhkniazi@gmail.com>
2023-01-01 21:12:54 +0500

    final commit

--- pages/posts/backend.js
+++ pages/posts/backend.js
@@ -7,7 +7,7 @@ const backend = ( props ) => {
       <main className='mt-10 w-full flex flex-col justify-center items-center mb-20'>
          <ul>
             {posts.map( ( post ) => (
-               <li className='w-[900px]'>
+               <li className='xs:w-[340px] lg:w-[550px] xl:w-[750px] xxl:w-[900px]'>
                   <PostItem key={post.slug} post={post} />
                </li>
             ) )}
